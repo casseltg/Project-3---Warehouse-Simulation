d03cfe29d25c4dc561a7e7bde7487d12cde3d689
Hope Cassel <113550924+casseltg@users.noreply.github.com>
2023-11-02 13:32:37 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_3___Warehouse_Simulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
