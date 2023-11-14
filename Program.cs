c7c0786dab7e1558c7e43b601cf726bc55b8ae92
GreenSerpent7 <136490293+GreenSerpent7@users.noreply.github.com>
2023-11-14 11:31:35 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
-﻿namespace Project_3___Warehouse_Simulation
+///////////////////////////////////////////////////////////////////////////////
+//
+// Author: Derek Depew, depewdj@mail.etsu.edu
+// Author: Hope Grayson,casseltg@mail.etsu.edu
+// Author: Kevin Davis, daviskl2@etsu.edu
+// Author: Venkata Sireesha Dulam,dulamv@etsu.edu
+// Course: CSCI-2210-001 - Data Structures
+// Assignment: Project 3: Warehouse Simulation
+// Description: This Project simulates the production profit margin for a Warehouse.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+namespace Project_3___Warehouse_Simulation
 {
     internal class Program
     {
